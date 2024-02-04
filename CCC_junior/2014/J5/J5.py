dc70aa12d1314dc10396e9065fd052b9d5557b81
DipperChen2007 <chenxingan65@gmail.com>
2024-02-04 14:59:29 -0800

    666

---
+++ CCC_junior/2014/J5/J5.py
@@ -0,0 +1,13 @@
+def Assigning_Partners(n,row_1,row_2):
+    dic  = {}
+    for i in range(n):
+        dic[row_1[i]] = row_2[i]
+        if dic[row_1[i]] == 
+
+def Take_input():
+    n = int(input())
+    row_1=((input().split()))
+    row_2=((input().split()))
+    return n,row_1,row_2
+
+print(Take_input())
