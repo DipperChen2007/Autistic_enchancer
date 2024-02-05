9eaf7d612b7bb4b59bc19bcedf85b86200afeb9a
DipperChen2007 <chenxingan65@gmail.com>
2024-02-05 12:48:42 -0800

    j

--- CCC_junior/2014/J5/J5.py
+++ CCC_junior/2014/J5/J5.py
@@ -1,15 +1,18 @@
 def Assigning_Partners(n,row_1,row_2):
-    dic = {}
+    dic_1 = {}
+    
     for i in range(n):
-        if row_1[i] not in dic:
-            dic[row_1[i]] = [row_2[i]]
-        elif row_1[i] in dic:
-            dic[row_1[i]].append(row_2[i])
+        if row_1[i] not in dic_1:
+            dic_1[row_1[i]] = [row_2[i]]
+        elif row_1[i] in dic_1:
+            dic_1[row_1[i]].append(row_2[i])
+        
+    
     for i_1 in range(n):
-        for i_2 in range(len(dic[row_1[i_1]])):
-            if row_1[i_1] not in dic[dic[row_1[i_1]][i_2]]:
+        for i_2 in range(len(dic_1[row_1[i_1]])):
+            if  row_1[i_1] not in dic_1[dic_1[row_1[i_1]][i_2]]:
                 return "bad"
-
+    
     return "good"
 
 def Take_input():
